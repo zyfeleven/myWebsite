34c34d88c12c88b944fd9f9b2fb9c623fffd32e3
zyfeleven <zyfeleven@gmail.com>
2023-09-15 15:18:14 -0400

    update router basename

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/resume">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
